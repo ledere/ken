aaf40d332a2a8c2905a3b17c0fb7867d9a6d7b78
ledere <frank@duckbite.nl>
2018-07-25 09:07:40 +0200

    interpret command line arguments

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 var natural = require("natural");
 var path = require("path");
 
+//console.log(process.argv);
+
+if (process.argv[2]) {
+	var sentence = process.argv[2];
+} else {
+	var sentence = "You forgot to enter an argument to this script";
+}
 
 var base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger");
 var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt";
@@ -11,8 +18,6 @@ var defaultCategory = 'N';
 var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory);
 var rules = new natural.RuleSet(rulesFilename);
 var tagger = new natural.BrillPOSTagger(lexicon, rules);
-
-var sentence = "I see the man with the telecope in his asshole";
 
 var tokenizer = new natural.WordTokenizer();
 var tokens = tokenizer.tokenize(sentence);
